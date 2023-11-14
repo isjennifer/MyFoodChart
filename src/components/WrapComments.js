6f8cf294715e842a5b8ece032322fe55069b2c4e
isjennifer <duensdl@gmail.com>
2023-11-14 22:35:34 +0900

    feat:wrapcommnets

--- src/components/WrapComments.js
+++ src/components/WrapComments.js
@@ -1,29 +1,35 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import CommentLists from "./CommentLists";
 
 
-export default function WrapComments({comments}) {
+export default function WrapComments() {
     const [input, setInput] = useState('')
-    const [commentLists, setCommentLists] = useState(comments)
-    const params = useParams();
-    const {id} = params;
+    const [commentLists, setCommentLists] = useState([{ id: 0, username: '', content: '', createdAt: '' }])
+    // const params = useParams();
+    // const {id} = params;
+    const nextID = useRef(1);
     const date = new Date();
 
+    // useEffect(() => {
+    //   setCommentLists()
+    // }, [commentLists]);
 
     const addComment = () => {
       if (input !== '') {
         const newComment = {
-          id: id,
+          id: nextID.current,
           username: 'dundun',
           content: input,
           createdAt: date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay()
         };
         setCommentLists([...commentLists, newComment]);
         setInput('');
+        nextID.current += 1; 
       }
     };
     
+
     const editComment = (commentId, editValue) => {
       let newCommentLists = commentLists?.map((item) =>{
         if (item.id===commentId) {
