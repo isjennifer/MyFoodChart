6aed04b8c738a97f621961fc73f86f0ae7747e6f
isjennifer <duensdl@gmail.com>
2023-11-15 23:42:23 +0900

    feat: 댓글 css 입히기

--- src/components/WrapComments.js
+++ src/components/WrapComments.js
@@ -1,33 +1,24 @@
-import { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useRef, useState } from "react";
 import CommentLists from "./CommentLists";
-
+import styled from "styled-components"
 
 export default function WrapComments() {
     const [input, setInput] = useState('')
     const [commentLists, setCommentLists] = useState([])
-
-    // const params = useParams();
-    // const {id} = params;
     const nextID = useRef(1);
-    const date = new Date();
-
-    // useEffect(() => {
-    //   setCommentLists()
-    // }, [commentLists]);
-
+    // 오늘 날짜 간편하게 받아오기 : Moment.js 
+    const moment = require('moment');
+    
     const addComment = () => {
       if (input !== '') {
         const newComment = {
           id: nextID.current,
           username: 'dundun',
           content: input,
-          createdAt: date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay()
+          createdAt: moment().format('YYYY-MM-DD')
         };
         let newCommentList = [...commentLists]
-        console.log(newCommentList);
         newCommentList.push(newComment);
-        console.log(newCommentList);
         setCommentLists(newCommentList);
         setInput('');
         nextID.current += 1; 
@@ -50,18 +41,67 @@ export default function WrapComments() {
     return (
       <>
         <CommentLists commentLists={commentLists} editComment={editComment} />
-        <div>
-          <input
+        <CommentBox>
+          <Input
             type="text"
             placeholder="댓글 달기..."
             value={input||''}
             onChange={e => setInput(e.target.value)}
             onKeyDown={e => (e.key === 'Enter' ? addComment() : null)}
             />
-          <button disabled="" onClick={addComment}>
+          <Button disabled="" onClick={addComment}>
               게시
-          </button>
-        </div>
+          </Button>
+        </CommentBox>
       </>
     )
   }
+
+
+
+  const Button = styled.button`
+    display: flex;
+    width: 70px;
+    height: 30px;
+    background-color: #F97F51;
+    color: white;
+    border: none;
+    justify-content: center;
+    align-items: center;
+    border-radius: 40px;
+    &:hover{
+        cursor: pointer;
+    }
+    
+    
+`
+
+  const Input = styled.input`
+    border: none; // 검색창 border 을 없앰으로써 자연스러워짐
+    -webkit-appearance: none; // 기본 search 디자인을 없앰
+    width: 100%;
+    height: 40px;
+    overflow: auto; //검색어가 길어졌을때 오른쪽으로 자연스럽게 검색되도록 하기 위해
+    font-size: 16px;
+    margin-left: 10px;
+    &:focus{
+        outline: none;
+    };
+`
+
+
+const CommentBox = styled.div`
+    display: inline-flex;
+    background-color: white;
+    border: #7B7B7B solid 1px;
+    padding: 0px 10px;
+    border-radius: 40px;
+    width: 100%;
+    height: 45px;
+    font-size: 16px;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 20px;
+
+
+`
