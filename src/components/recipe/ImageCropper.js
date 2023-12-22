9e9a4fb171f781f097dc00c01570b5e92e282290
isjennifer <duensdl@gmail.com>
2023-12-23 00:57:12 +0900

    개인정보수정 페이지에 프로필 사진 변경 기능 추가

--- src/components/recipe/ImageCropper.js
+++ src/components/recipe/ImageCropper.js
@@ -1,13 +1,11 @@
-
-import styled from "styled-components"
+import styled from "styled-components";
 import { useRef, useState } from "react";
 import { Cropper } from "react-cropper";
 import "cropperjs/dist/cropper.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCrop } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCrop } from "@fortawesome/free-solid-svg-icons";
 
-
-const ImageCropper = ({ children, onCrop }) => {
+const ImageCropper = ({ children, onCrop, aspectRatio }) => {
   const inputRef = useRef(null);
   const cropperRef = useRef(null);
   const [image, setImage] = useState(null);
@@ -25,13 +23,12 @@ const ImageCropper = ({ children, onCrop }) => {
 
     const reader = new FileReader();
     reader.onload = () => {
-        if (reader.readyState === FileReader.DONE) {
-            setImage(String(reader.result));
-          }
+      if (reader.readyState === FileReader.DONE) {
+        setImage(String(reader.result));
+      }
     };
     reader.readAsDataURL(files[0]);
   };
-
 
   const getCropData = () => {
     if (typeof cropperRef.current?.cropper !== "undefined") {
@@ -39,7 +36,6 @@ const ImageCropper = ({ children, onCrop }) => {
       setImage(null);
     }
   };
-
 
   return (
     <>
@@ -49,34 +45,37 @@ const ImageCropper = ({ children, onCrop }) => {
         style={{ display: "none" }}
         onChange={handleFileChange}
       />
-      <span onClick={handleChildrenClick}>
-        {children}
-      </span>
+      <span onClick={handleChildrenClick}>{children}</span>
       {image && (
         <ModalBox>
           <Modal>
-          <div>
-            <h1 style={{marginBottom:15}}><FontAwesomeIcon icon={faCrop} style={{marginRight:10}}/>이미지 자르기</h1>
             <div>
+              <h1 style={{ marginBottom: 15 }}>
+                <FontAwesomeIcon icon={faCrop} style={{ marginRight: 10 }} />
+                이미지 자르기
+              </h1>
+              <div>
                 <Cropper
-                   ref={cropperRef}
-                   aspectRatio={1.6}
-                   src={image}
-                   viewMode={1}
-                   width={800}
-                   height={500}
-                   background={false}
-                   responsive
-                   autoCropArea={1}
-                   checkOrientation={false}
-                   guides
+                  ref={cropperRef}
+                  aspectRatio={aspectRatio}
+                  src={image}
+                  viewMode={1}
+                  width={800}
+                  height={500}
+                  background={false}
+                  responsive
+                  autoCropArea={1}
+                  checkOrientation={false}
+                  guides
                 />
-            </div>
-            <div style={{display:"flex", float:"right"}}>
-                <Button onClick={getCropData} style={{marginRight:15}}>적용하기</Button>
+              </div>
+              <div style={{ display: "flex", float: "right" }}>
+                <Button onClick={getCropData} style={{ marginRight: 15 }}>
+                  적용하기
+                </Button>
                 <Button onClick={() => setImage(null)}>취소</Button>
+              </div>
             </div>
-          </div>
           </Modal>
         </ModalBox>
       )}
@@ -86,46 +85,42 @@ const ImageCropper = ({ children, onCrop }) => {
 
 export default ImageCropper;
 
-
-
-
 const Button = styled.div`
-    display: flex;
-    width: 100px;
-    height: 50px;
-    border: solid 2px #505050;
-    color: #505050;
-    margin: 15px 0px 0px 0px;
-    justify-content: center;
-    align-items: center;
-    border-radius: 10px;
-    &:hover{
-        cursor: pointer;
-    }
-    
-`
+  display: flex;
+  width: 100px;
+  height: 50px;
+  border: solid 2px #505050;
+  color: #505050;
+  margin: 15px 0px 0px 0px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const ModalBox = styled.div`
-    display: flex;
-    position: fixed;
-    z-index:999;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0,0,0,0.2);
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  justify-content: center;
+  align-items: center;
+`;
 
 const Modal = styled.div`
-    display: flex;
-    position: absolute;
-    z-index: 1000;
-    width: 850px;
-    height: 650px;
-    background-color: white;
-    border-radius: 10px;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  position: absolute;
+  z-index: 1000;
+  width: 850px;
+  height: 650px;
+  background-color: white;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+`;
